601c70408b9051f97ed9c894582b538f62678c6d
nvb-mp4 <timofejkuderskij673@gmail.com>
2025-12-07 14:50:00 +0300

    fixes

--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -2,7 +2,7 @@ import styles from "./MyButton.module.css";
 
 export const MyButton = ({children, ...props}) => {
   return (
-    <button {...props} className={styles.myBtn}>
+    <button {...props} className={styles.myButton}>
       {children}
     </button>
   );
